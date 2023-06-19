486fcedf41baf0846ed5df769ad874fcb0a45286
OrbyyOrbinaut <77822077+OrbyyOrbinaut@users.noreply.github.com>
2023-06-18 23:15:24 -0500

    im pushin out more kids

--- assets/preload/stages/alley.lua
+++ assets/preload/stages/alley.lua
@@ -15,17 +15,40 @@ function onCreate()
 	setScrollFactor('Garbage Group 2', 1.1, 1.0);
 	scaleObject('Garbage Group 2', 1.0, 1.0);
 	
+	makeLuaSprite('blue', nil, -500, -100);
+	makeGraphic('blue', 2500, 1080, 'B7BBF0')
+	setBlendMode('blue', 'multiply')
+	
+	makeAnimatedLuaSprite('rains', 'botfriend/rain', -2000, -1500);
+	setScrollFactor('rains', 1.0, 1.0);
+	scaleObject('rains', 1.0, 1.0);
+	addAnimationByPrefix('rains', 'raining', 'Rain', 24, true)
+	playAnim('rains', 'raining')
+	setObjectCamera('rains', 'hud')
+	setProperty('rains.alpha', 0.20);
+	setBlendMode('rains', 'add')
+	
 	addLuaSprite('Wall', false);
 	addLuaSprite('Garbage Bin', false);
 	addLuaSprite('Garbage Group 1', true);
 	addLuaSprite('Garbage Group 2', true);
 end
 
+function onCreatePost()--rain is there from the start on future link
+    if songPath == 'future-link' then
+	addLuaSprite('blue', true);
+	addLuaSprite('rains', true);
+    end
+end
 
 function onUpdate(elapsed)
 if curStep > 239 and curStep < 250 and songName == 'Cyberbolt' then
 setProperty('camZooming',false)
-elseif curStep > 250 and songName == 'Cyberbolt' then
+elseif curStep > 250 and curStep < 765 and songName == 'Cyberbolt' then
+setProperty('camZooming', true)
+elseif curStep > 765 and curStep < 782 and songName == 'Cyberbolt' then
+setProperty('camZooming',false)
+elseif curStep > 782 and songName == 'Cyberbolt' then
 setProperty('camZooming', true)
 end
-end
+end 
