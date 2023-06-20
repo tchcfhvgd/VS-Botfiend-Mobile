8fb54a5382473be1e9127188cdbe409d777c8583
OrbyyOrbinaut <77822077+OrbyyOrbinaut@users.noreply.github.com>
2023-06-20 13:52:52 -0500

    credits

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,29 +81,14 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-			['Psych Engine Team'],
-			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',								'https://twitter.com/Shadow_Mario_',	'444444'],
-			['RiverOaken',			'river',			'Main Artist/Animator of Psych Engine',							'https://twitter.com/RiverOaken',		'B42F71'],
-			['shubs',				'shubs',			'Additional Programmer of Psych Engine',						'https://twitter.com/yoshubs',			'5E99DF'],
-			[''],
-			['Former Engine Members'],
-			['bb-panzu',			'bb',				'Ex-Programmer of Psych Engine',								'https://twitter.com/bbsub3',			'3E813A'],
-			[''],
-			['Engine Contributors'],
-			['iFlicky',				'flicky',			'Composer of Psync and Tea Time\nMade the Dialogue Sounds',		'https://twitter.com/flicky_i',			'9E29CF'],
-			['SqirraRNG',			'sqirra',			'Crash Handler and Base code for\nChart Editor\'s Waveform',	'https://twitter.com/gedehari',			'E1843A'],
-			['EliteMasterEric',		'mastereric',		'Runtime Shaders support',										'https://twitter.com/EliteMasterEric',	'FFBD40'],
-			['PolybiusProxy',		'proxy',			'.MP4 Video Loader Library (hxCodec)',							'https://twitter.com/polybiusproxy',	'DCD294'],
-			['KadeDev',				'kade',				'Fixed some cool stuff on Chart Editor\nand other PRs',			'https://twitter.com/kade0912',			'64A250'],
-			['Keoiki',				'keoiki',			'Note Splash Animations',										'https://twitter.com/Keoiki_',			'D2D2D2'],
-			['Nebula the Zorua',	'nebula',			'LUA JIT Fork and some Lua reworks',							'https://twitter.com/Nebula_Zorua',		'7D40B2'],
-			['Smokey',				'smokey',			'Sprite Atlas Support',											'https://twitter.com/Smokey_5_',		'483D92'],
-			[''],
-			["Funkin' Crew"],
-			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",							'https://twitter.com/ninja_muffin99',	'CF2D2D'],
-			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",								'https://twitter.com/PhantomArcade3K',	'FADC45'],
-			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",								'https://twitter.com/evilsk8r',			'5ABD4B'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",								'https://twitter.com/kawaisprite',		'378FC7']
+			['VS Botfiend Team'],
+			['Humbay',		'humbay',		'Creator, Main Artist/Animator',								'https://twitter.com/HumbayNoob',	'C0140D'],
+			['Jbug',			'jbug',			'Designer of Botfiend',							'https://www.youtube.com/@JBUG/',		'FFFFFF'],
+			['Orbyy',			'orbyy',			'Coder and Charter',							'https://twitter.com/OrbyyNew',		'74C6E4'],
+			['Krea',				'krea',			'Composed Cyberbolt and Future Link',						'https://twitter.com/Kreagato',			'805228'],
+			['Toad',				'toad',			'Composed Kilowatt',						'https://twitter.com/OdysseyToad',			'A4FFFF'],
+			['Keegan',				'keegan',			'Composed Puttin',						'https://twitter.com/__Keegan_',			'FF3300'],
+			['Komie',				'komie',			'Logo designer',						'https://twitter.com/whoisKomica',			'FF5300']
 		];
 		
 		for(i in pisspoop){
